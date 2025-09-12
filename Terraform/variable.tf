030bef538ad6ddea673dfee953f43606192b68ed
g742 <61184350+g742@users.noreply.github.com>
2025-09-12 21:51:18 +0530

    Create variable.tf

---
+++ Terraform/variable.tf
@@ -0,0 +1,44 @@
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "availability_zone" {
+  default = "us-east-2a"
+}
+
+variable "tag" {
+  default = "Web-sg"
+}
+
+variable "ami" {
+  default = "ami-0ea3c35c5c3284d82"
+}
+
+variable "instance" {
+  default = "t2.micro"
+}
+
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "availability_zone" {
+  default = "us-east-2a"
+}
+
+variable "tag" {
+  default = "Web-sg"
+}
+
+variable "ami" {
+  default = "ami-0ea3c35c5c3284d82"
+}
+
+variable "access" {
+  default = "AKIAZI34YLAX6XXXID67"
+}
+
+variable "secret" {
+  default = "FuvE41qRraDGIRb36fMjsjWwgnYNQSj5Si3U6NN8"
+}
+
